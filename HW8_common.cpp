8be53370eea2a9d81f8910d3884d470d92d91d6b
showgan <rexx0520@gmail.com>
2018-12-03 23:14:09 +0800

    HW8 救人用

---
+++ HW8_common.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <cstdlib> 
+#include <string>
+using namespace std;
+
+struct Node{
+    string data;
+    Node *nextNode = nullptr;
+};
+
+struct Nodehead {
+    string data;
+    Node *nextNode = nullptr;
+
+    void push(string newData) {
+        if (data.empty()) {
+            data = newData;
+            return;
+        }
+
+        if (nextNode == nullptr){
+            nextNode = new Node{newData};
+            return;
+        }
+
+        Node *lastNode = nextNode;
+        while (lastNode->nextNode != nullptr) {
+            lastNode = lastNode->nextNode;
+        }
+        lastNode->nextNode = new Node{newData};
+    }
+
+    void pop(){
+        if (nextNode == nullptr) {
+            data = "";
+            return;
+        }
+        
+        if (nextNode->nextNode == nullptr) {
+            delete nextNode;
+            nextNode = nullptr;
+            return;
+        }
+
+        Node *secondLastNode = nextNode;
+        Node *lastNode = nextNode->nextNode;
+        while (lastNode->nextNode != nullptr) {
+            secondLastNode = lastNode;
+            lastNode = lastNode->nextNode;
+        }
+        delete lastNode;
+        secondLastNode->nextNode = nullptr;
+    }
+
+    void show(){
+        if (!data.empty()) cout << "<" << data << ">";
+
+        Node *currentNode = nextNode;
+        while (currentNode != nullptr) {
+            cout << "<" << currentNode->data << ">";
+            currentNode = currentNode->nextNode;
+        }
+        cout << endl;
+    }
+} first;
+
+int main (){
+	cout<<"Input:";
+    string input;
+    getline(cin,input);
+
+    for (int i=0;i<=input.size();i++){
+        char self = input[i];
+        string operation;
+        string target;
+
+        if (self == '<') { // 遇到 '<'，開始儲存指令
+            i++;
+            self = input[i];
+            while (self != ' ' && self != '>') { // 遇到空白或>之前都當作指令儲存在 operation 裡面
+                operation += self;
+                i++;
+                self = input[i];
+            } // 此時指令儲存完成，self == ' ' 或 self == '>' 有一個會成立
+
+            if (self == ' ') {
+                i++; // 跳過空白
+                self = input[i];
+
+                while (self != '>') { // 在遇到>之前都當作要儲存的東西
+                    target += self;
+                    i++;
+                    self = input[i];
+                }
+            } // 此時 target 是要儲存的目標, self == '>'。
+
+            if (operation == "push") {
+                first.push(target);         // 做 push
+            } else if (operation == "pop"){ // 此時 self == '>', operation == "pop"
+                first.pop();                // 做 pop
+            } else if (operation == "show"){ // 此時 self == '>', operation == "show"
+                first.show();                // 做 show
+            }
+        }
+	}
+    return 0;
+}
